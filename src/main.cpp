c6987c40f2e2d907726c5aa3a496db18d41b4c60
gorkov0610 <gorkov.hristijan@uklo.edu.mk>
2026-01-16 17:43:21 +0100

    Commented the code

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 #include <cstdint>
 #include <iomanip>
 
+//Shift ammounts
 constexpr uint32_t S[64]{
     7,12,17,22, 7,12,17,22, 7,12,17,22, 7,12,17,22,
     5, 9,14,20, 5, 9,14,20, 5, 9,14,20, 5, 9,14,20,
     4,11,16,23, 4,11,16,23, 4,11,16,23, 4,11,16,23,
     6,10,15,21, 6,10,15,21, 6,10,15,21, 6,10,15,21
 };
+//Sine constants
 constexpr uint32_t T[64] = {
     0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
     0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
@@ -32,6 +34,7 @@ uint32_t B = 0xefcdab89;
 uint32_t C = 0x98badcfe;
 uint32_t D = 0x10325476;
 
+//Feistel functions
 uint32_t F(uint32_t x, uint32_t y, uint32_t z){
     return (x & y) | (~x & z);
 }
@@ -44,29 +47,37 @@ uint32_t H(uint32_t x, uint32_t y, uint32_t z){
 uint32_t I(uint32_t x, uint32_t y, uint32_t z){
     return y ^ (x | ~z);
 }
+//Rotate bits left
 uint32_t rot_left(uint32_t x, uint32_t s){
     return (x << s) | (x >> (32 - s));
 }
 
 
 int main(){
+    //Get the plaintext input
     std::string input;
     std::getline(std::cin, input);
     
+    //Pad the input with 0b10000000 to mark the end of the message
     uint64_t original_bits = input.size() * 8;
     input += '\x80';
 
+    //Pad zeros until the length is equal to 56 bytes
     size_t pad_bytes = (56 - (input.size() % 64) + 64) % 64;
     input.append(pad_bytes, '\0');
 
+    //Append the original length in little endian
     for(auto i{0}; i < 8; i++){
         input += static_cast<char>((original_bits >> (8 * i)) & 0xFF);
     }
 
+    //Main hash cycle
     for(auto i{0}; i < input.size(); i += 64){
 
+        //Copy the values from the previous round
         uint32_t AA = A, BB = B, CC = C, DD = D;
 
+        //Get the input message as 64 byte little endian
         uint32_t X[16];
         for(auto j{0}; j < 16; j++){
             X[j] = 
@@ -76,7 +87,7 @@ int main(){
             ((uint32_t)(unsigned char)input[i + j * 4 + 3] << 24);
         }
 
-        
+        //Use the Feistel functions to get the vallues for this round
         for(auto k{0}; k < 64; k++){
             uint32_t f,g;
 
@@ -111,6 +122,7 @@ int main(){
     uint32_t hash[4] = {A, B, C, D};
     uint8_t bytes[16];
 
+    //Convert the A, B, C and D state words into 16 bytes little-endian order
     for(auto i{0}; i < 4; i++){
         bytes[i * 4] = hash[i] & 0xFF;
         bytes[i * 4 + 1] = (hash[i] >> 8) & 0xFF;
@@ -118,8 +130,10 @@ int main(){
         bytes[i * 4 + 3] = (hash[i] >> 24) & 0xFF;
     }
 
+    //Setup the output for printing
     std::cout << std::hex << std::uppercase << std::setfill('0');
 
+    //Print the hash
     for(auto i{0}; i < 16; i++){
         std::cout << std::setw(2) << static_cast<unsigned>(bytes[i]);
     }
